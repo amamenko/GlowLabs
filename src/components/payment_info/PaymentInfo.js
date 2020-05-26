c4bc908ea6599f54cacc3ffcc90059682e3c17c2
Avraham Mamenko <amamenko@hotmail.com>
2020-05-25 23:59:42 -0400

    Added additional payment info form features including stored credit card selection ability for returning clients

--- src/components/payment_info/PaymentInfo.js
+++ src/components/payment_info/PaymentInfo.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect, useCallback } from "react";
 import { useSelector } from "react-redux";
 import {
   SquarePaymentForm,
@@ -12,12 +12,14 @@ import "./PaymentInfo.css";
 import {
   faChevronLeft,
   faChevronRight,
+  faChevronCircleDown,
 } from "@fortawesome/free-solid-svg-icons";
 import { Redirect, Link } from "react-router-dom";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import axios from "axios";
 import { updateClientSquareIDMutation } from "../../graphql/queries/queries";
 import { useMutation } from "@apollo/react-hooks";
+import { FormGroup, Label, Input } from "reactstrap";
 
 const PaymentInfo = (props) => {
   const userAuthenticated = useSelector(
@@ -37,11 +39,85 @@ const PaymentInfo = (props) => {
   const [errorMessages, changeErrorMessage] = useState([]);
   const [cardHolderFirstName, changeCardHolderFirstName] = useState("");
   const [cardHolderLastName, changeCardHolderLastName] = useState("");
+  const [squareStoredCreditCards, changeSquareStoredCreditCards] = useState("");
+  const [selectedCreditCard, changeSelectedCreditCard] = useState("");
+  const [
+    selectedCreditCardFullData,
+    changeSelectedCreditCardFullData,
+  ] = useState("");
+
+  useEffect(() => {
+    if (squareStoredCreditCards) {
+      if (selectedCreditCard) {
+        const creditCardSplitArr = selectedCreditCard.split(" ");
+
+        const fullData = squareStoredCreditCards.data.find((x) => {
+          return (
+            x.card_brand === creditCardSplitArr[0] &&
+            x.last_4 === creditCardSplitArr[creditCardSplitArr.length - 1]
+          );
+        });
+
+        if (fullData) {
+          changeSelectedCreditCardFullData(fullData);
+        }
+      }
+    }
+  }, [selectedCreditCard, squareStoredCreditCards]);
+
+  console.log(selectedCreditCardFullData);
 
   const [
     updateClientSquareID,
     { data: updateClientSquareIDData },
   ] = useMutation(updateClientSquareIDMutation);
+
+  const retrieveSquareCustomerFunction = useCallback(() => {
+    return axios.post(
+      "http://localhost:4000/retrieve_customer",
+      {
+        data: {
+          squareCustomerId: props.getClientData.client.squareCustomerId,
+        },
+      },
+      {
+        headers: {
+          Authorization:
+            "Bearer " + process.env.REACT_APP_SQUARE_SANDBOX_ACCESS_TOKEN,
+        },
+      }
+    );
+  }, [props.getClientData]);
+
+  useEffect(() => {
+    if (userAuthenticated) {
+      if (!squareStoredCreditCards) {
+        const fetchData = async () => {
+          const customerData = await retrieveSquareCustomerFunction();
+
+          changeSquareStoredCreditCards(customerData);
+        };
+
+        fetchData();
+      }
+    }
+  }, [
+    userAuthenticated,
+    retrieveSquareCustomerFunction,
+    squareStoredCreditCards,
+  ]);
+
+  const renderStoredCreditCardOptions = () => {
+    if (squareStoredCreditCards) {
+      return squareStoredCreditCards.data.map((x, i) => {
+        return (
+          <option onClick={() => changeSelectedCreditCard(x.id)} key={i}>
+            {x.card_brand + " - " + x.last_4}
+          </option>
+        );
+      });
+    }
+  };
 
   const cardNonceResponseReceived = (
     errors,
@@ -79,53 +155,77 @@ const PaymentInfo = (props) => {
           : phoneNumber,
       };
 
-      axios
-        .post("http://localhost:4000/customers", squareCustomerData, {
-          headers: {
-            Authorization:
-              "Bearer " + process.env.REACT_APP_SQUARE_SANDBOX_ACCESS_TOKEN,
-          },
-        })
-        .then((res) => {
-          const squareData = {
-            card_nonce: nonce,
-            billing_address: { postal_code: cardData.billing_postal_code },
-            cardholder_name:
-              (userAuthenticated
-                ? cardHolderFirstName
+      const squarePostRequestFunction = () => {
+        axios
+          .post("http://localhost:4000/customers", squareCustomerData, {
+            headers: {
+              Authorization:
+                "Bearer " + process.env.REACT_APP_SQUARE_SANDBOX_ACCESS_TOKEN,
+            },
+          })
+          .then((res) => {
+            const squareData = {
+              card_nonce: nonce,
+              billing_address: { postal_code: cardData.billing_postal_code },
+              cardholder_name:
+                (userAuthenticated
                   ? cardHolderFirstName
-                  : props.getClientData.client.firstName
-                : cardHolderFirstName
-                ? cardHolderFirstName
-                : firstName
-              ).trim() +
-              " " +
-              (userAuthenticated
-                ? cardHolderLastName
+                    ? cardHolderFirstName
+                    : props.getClientData.client.firstName
+                  : cardHolderFirstName
+                  ? cardHolderFirstName
+                  : firstName
+                ).trim() +
+                " " +
+                (userAuthenticated
                   ? cardHolderLastName
-                  : props.getClientData.client.lastName
-                : cardHolderLastName
-                ? cardHolderLastName
-                : lastName
-              ).trim(),
-            verification_token: buyerVerificationToken,
-            customerId: JSON.parse(res.request.response).customer.id,
-          };
+                    ? cardHolderLastName
+                    : props.getClientData.client.lastName
+                  : cardHolderLastName
+                  ? cardHolderLastName
+                  : lastName
+                ).trim(),
+              verification_token: buyerVerificationToken,
+              customerId: JSON.parse(res.request.response).customer.id,
+            };
 
-          return axios.post(
-            "http://localhost:4000/customers/card",
-            squareData,
-            {
-              headers: {
-                Authorization:
-                  "Bearer " + process.env.REACT_APP_SQUARE_SANDBOX_ACCESS_TOKEN,
+            updateClientSquareID({
+              variables: {
+                squareCustomerId: JSON.parse(res.request.response).customer.id,
+                firstName: userAuthenticated
+                  ? props.getClientData.client.firstName
+                  : firstName,
+                lastName: userAuthenticated
+                  ? props.getClientData.client.lastName
+                  : lastName,
+                email: userAuthenticated
+                  ? props.getClientData.client.email
+                  : email,
               },
-            }
-          );
-        })
-        .catch((err) => {
-          console.error(err);
-        });
+            });
+
+            return axios.post(
+              "http://localhost:4000/customers/card",
+              squareData,
+              {
+                headers: {
+                  Authorization:
+                    "Bearer " +
+                    process.env.REACT_APP_SQUARE_SANDBOX_ACCESS_TOKEN,
+                },
+              }
+            );
+          })
+          .catch((err) => {
+            console.error(err);
+          });
+      };
+
+      userAuthenticated
+        ? props.getClientData.client.squareCustomerId
+          ? retrieveSquareCustomerFunction()
+          : squarePostRequestFunction()
+        : squarePostRequestFunction();
     }
   };
 
@@ -171,6 +271,37 @@ const PaymentInfo = (props) => {
     changeCardHolderLastName(e.currentTarget.value.toUpperCase().trim());
   };
 
+  useEffect(() => {
+    const iFrameLabel = [...document.getElementsByClassName("sq-label")].filter(
+      (x) => x.innerText === "CREDIT CARD"
+    );
+    const iFrameLabelHidden = [
+      ...document.getElementsByClassName("sq-label"),
+    ].filter((x) => x.className.includes("sq-payment-form-hidden"));
+
+    if (selectedCreditCardFullData) {
+      document
+        .getElementById("sq-payment-form-sq-card-number")
+        .classList.add("sq-payment-form-hidden");
+
+      if (iFrameLabel) {
+        if (iFrameLabel[0]) {
+          iFrameLabel[0].classList.add("sq-payment-form-hidden");
+        }
+      }
+    } else {
+      document
+        .getElementById("sq-payment-form-sq-card-number")
+        .classList.remove("sq-payment-form-hidden");
+
+      if (iFrameLabelHidden) {
+        if (iFrameLabelHidden[0]) {
+          iFrameLabelHidden[0].classList.remove("sq-payment-form-hidden");
+        }
+      }
+    }
+  }, [selectedCreditCardFullData]);
+
   return (
     <div className="payment_info_container">
       {redirectToHome()}
@@ -207,6 +338,34 @@ const PaymentInfo = (props) => {
           className="square_payment_form"
         >
           <fieldset className="sq-fieldset">
+            <FormGroup className="choose_a_credit_card_formgroup">
+              <Label className="sq-label" for="select">
+                CHOOSE A CREDIT CARD
+              </Label>
+              <FontAwesomeIcon
+                className="choose_a_credit_card_dropdown_icon"
+                icon={faChevronCircleDown}
+              />
+              <Input
+                className="sq-cardholder-input"
+                type="select"
+                name="select"
+                id="select"
+                onChange={(e) => {
+                  if (selectedCreditCard) {
+                    if (e.target.value === "NEW CARD") {
+                      changeSelectedCreditCard("");
+                      changeSelectedCreditCardFullData("");
+                    }
+                  } else {
+                    changeSelectedCreditCard(e.target.value);
+                  }
+                }}
+              >
+                <option>NEW CARD</option>
+                {renderStoredCreditCardOptions()}
+              </Input>
+            </FormGroup>
             <div className="sq_card_holder_container">
               <span className="sq-label">CARDHOLDER FIRST NAME</span>
               <input
@@ -215,8 +374,13 @@ const PaymentInfo = (props) => {
                 maxLength="100"
                 placeholder="Enter cardholder first name here"
                 className="sq-cardholder-input"
+                disabled={selectedCreditCardFullData ? true : false}
                 onChange={handleCardHolderFirstName}
-                value={cardHolderFirstName}
+                value={
+                  selectedCreditCardFullData
+                    ? selectedCreditCardFullData.cardholder_name.split(" ")[0]
+                    : cardHolderFirstName
+                }
               />
             </div>
             <div className="sq_card_holder_container">
@@ -227,10 +391,36 @@ const PaymentInfo = (props) => {
                 maxLength="100"
                 placeholder="Enter cardholder last name here"
                 className="sq-cardholder-input"
+                disabled={selectedCreditCardFullData ? true : false}
                 onChange={handleCardHolderLastName}
-                value={cardHolderLastName}
+                value={
+                  selectedCreditCardFullData
+                    ? selectedCreditCardFullData.cardholder_name.split(" ")[
+                        selectedCreditCardFullData.cardholder_name.split(" ")
+                          .length - 1
+                      ]
+                    : cardHolderLastName
+                }
               />
             </div>
+            {selectedCreditCardFullData ? (
+              <div className="sq_card_holder_container">
+                <span className="sq-label">CREDIT CARD </span>
+                <input
+                  name="credit_card"
+                  type="text"
+                  maxLength="100"
+                  className="sq-cardholder-input"
+                  disabled={selectedCreditCardFullData ? true : false}
+                  value={
+                    selectedCreditCardFullData.card_brand.toLowerCase() ===
+                    "americanexpress"
+                      ? "•••• •••••• •" + selectedCreditCardFullData.last_4
+                      : "•••• •••• •••• " + selectedCreditCardFullData.last_4
+                  }
+                />
+              </div>
+            ) : null}
             <CreditCardNumberInput />
             <div className="sq_third_container">
               <div className="sq-form-third_credit_card">
