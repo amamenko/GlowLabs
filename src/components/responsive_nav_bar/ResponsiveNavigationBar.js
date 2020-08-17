828c6884f344b8d95bfb918efbe0006fb137b0ce
amamenko <amamenko@hotmail.com>
2020-08-17 19:42:08 -0400

    Added responsive landing page and card styles, as well as additional footer styling

--- src/components/responsive_nav_bar/ResponsiveNavigationBar.js
+++ src/components/responsive_nav_bar/ResponsiveNavigationBar.js
@@ -875,24 +875,6 @@ const ResponsiveNavigationBar = React.forwardRef((props, ref) => {
           className="nav_login_container"
           onClick={handleLogoutClicked}
           style={{
-            top:
-              props.currentScreenSize === ""
-                ? props.initialScreenSize <= 1000 &&
-                  props.initialScreenSize >= 600
-                  ? window.matchMedia("(orientation: landscape)").matches
-                    ? props.scrollValue <= 5
-                      ? "0.6rem"
-                      : "1rem"
-                    : "auto"
-                  : "auto"
-                : props.currentScreenSize <= 1000 &&
-                  props.currentScreenSize >= 600
-                ? window.matchMedia("(orientation: landscape)").matches
-                  ? props.scrollValue <= 5
-                    ? "0.6rem"
-                    : "1rem"
-                  : "auto"
-                : "auto",
             background:
               props.currentScreenSize === ""
                 ? props.initialScreenSize >= 768 &&
