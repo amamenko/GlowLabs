06189396eb5772c877ca3cbcd40d0a7d1b1480d0
amamenko <amamenko@hotmail.com>
2020-12-28 20:20:52 -0500

    Added path import on server

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const path = require("path");
 const compression = require("compression");
 const { graphqlHTTP } = require("express-graphql");
 const expressPlayground = require("graphql-playground-middleware-express")
